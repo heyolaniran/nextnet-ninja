cc3b9b213253d9092537b8ced871fe7d25a73658
heyolaniran <olaniranlaurent@gmail.com>
2023-03-13 13:33:50 +0100

    feat: getStaticProps nextJs fetching data

--- src/pages/ninjas/index.jsx
+++ src/pages/ninjas/index.jsx
@@ -1,7 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
 
+export const getStaticProps = async () => {
 
-export default function Ninjas () {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users") ; 
+    const data = await res.json() ; 
+
+    return {
+        props: {ninjas: data}
+    }
+}
+
+export default function Ninjas ({ ninjas }) {
 
     return (
         <>
@@ -9,7 +19,29 @@ export default function Ninjas () {
                 <title>NextNet-Ninja | List </title>
             </Head>
             <div>
-                <h1>Ninjas Page </h1>
+                <h1 className="mb-6 text-center font-bold text-2xl">Ninjas List  </h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                    {ninjas.map((ninja) => (
+                      <div
+                        className="rounded-2xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 shadow-xl"
+                        key={ninja.id}
+                      >
+                            <Link className="block rounded-xl bg-white p-4 sm:p-6 lg:p-8" href="">
+                            <div className="mt-16" >
+                                <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+                                 { ninja.name }
+                                </h3>
+                        
+                                <p className="mt-2 text-sm text-gray-500">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, adipisci.
+                                </p>
+                            </div>
+                            </Link>
+                      </div>
+                      
+                    ))}
+                </div>
             </div>
         </>
         
